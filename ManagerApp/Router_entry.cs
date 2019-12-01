6ebb60f17fc9f9c97677d95fb362ec3292c8d9be
mkrzem-w <58052603+mkrzem-w@users.noreply.github.com>
2019-12-01 16:43:59 +0100

    Kod Systemu zarządzania

---
+++ ManagerApp/Router_entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ManagerApp
+{
+    class Router_entry
+    {
+        public String Router_id;
+        public EndPoint Router_address;
+
+        public Router_entry(String a, EndPoint b) {
+            Router_id = a;
+            Router_address = b;
+        }
+    }
+
+
+}
